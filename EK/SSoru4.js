04df1d39ae9489f038004a3a5496a8f1a8512358
Esaner <esaner@Emirhan-MacBook-Air.local>
2025-11-07 20:15:44 +0300

    20 TypeScript sorusu ve README eklendi

--- EK/SSoru4.js
+++
@@ -1,6 +0,0 @@
-function getProperty(obj, key) {
-    return obj[key];
-}
-var user = { name: "Ebubekir", age: 42 };
-console.log(getProperty(user, "name"));
-console.log(getProperty(user, "age"));
--- EK/SSoru8.js
+++
@@ -1,9 +0,0 @@
-function sum() {
-    var numbers = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        numbers[_i] = arguments[_i];
-    }
-    return numbers.reduce(function (total, n) { return total + n; }, 0);
-}
-console.log(sum(1, 2, 3));
-console.log(sum(10, 20, 30, 40));
